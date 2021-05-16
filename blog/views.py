cc13ec53481479114c90c4cd3b6f14149a2f75f4
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-05-16 15:56:08 +0700

    Make it better

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,18 @@
 #mysite/blog/views.py
 from django.shortcuts import render
-
+from .models import Post
 
 def index(request):
     from .controllers import _index
     kdict = _index.call()
-    return render(request, 'base.html', context=kdict)
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    posts = [post for post in queryset]
+    kdict['posts'] = posts
+    return render(request, 'index.html', context=kdict)
 
 
 def bookCover(request):
     from .controllers import _bookCover
     kdict = _bookCover.call()
     return render(request, 'bookcover.html', context=kdict)
-
 
